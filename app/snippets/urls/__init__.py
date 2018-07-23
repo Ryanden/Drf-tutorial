799aeca3136afeb8717de60309f9d0c4583847de
Ryanden <lockstom@gmail.com>
2018-07-23 16:52:55 +0900

    add mixin view

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -1,10 +1,11 @@
 
 from django.urls import path, include
-from . import django_view, api_view
+from . import django_view, api_view, mix_in
 
 app_name = 'snippets'
 
 urlpatterns = [
     path('django_view/', include(django_view)),
     path('api_view/', include(api_view)),
+    path('mix_in_view/', include(mix_in)),
 ]
